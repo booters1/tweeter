4c10204d1044877bd4c344df5d3c2ca6987e0e94
Ryan Huynh <rhuynh7@hotmail.com>
2024-03-24 23:11:52 -0600

    creating the tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,16 @@
+//reference: https://stackoverflow.com/a/7934560
 $(document).ready(function() {
-  //reference: https://stackoverflow.com/a/7934560
-  // attaching keyup event handeler 
   $(".new-tweet textarea").on("keyup", function() {
     //retrieve length of ea line
-      var inputLength = $(this).val().length;
-      var remainingChars = 140 - inputLength;
-      // updates counter after subtracting
-      $(".counter").text(remainingChars);
+    var inputLength = $(this).val().length;
+    var remainingChars = 140 - inputLength;
+    var counter = $(this).closest('form').find('.counter');
+    counter.text(remainingChars);
 
-
-      if (remainingChars < 0) {
-          $(".counter").addClass("over-limit");
-      } else {
-          $(".counter").removeClass("over-limit");
-      }
+    if (remainingChars < 0) {
+      counter.addClass("over-limit");
+    } else {
+      counter.removeClass("over-limit");
+    }
   });
 });
