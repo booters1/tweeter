f2875c1c0426202a2c39cbcd35848d225fffede4
Ryan Huynh <rhuynh7@hotmail.com>
2024-03-26 06:02:47 -0600

    fixed ai bot suggestions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
     })
     .done(function(response) {
         renderTweets([response]);
+        $('textarea').val('');
     })
     .fail(function(error) {
         console.error('tweet cannot be posted', error);
@@ -35,7 +36,7 @@ $(document).ready(function() {
                   <p>${tweetData.content.text}</p>
               </section>
               <footer>
-                  <p class="tweet-time">${tweetData.created_at}</p>
+                  <p class="tweet-time">${timeago.format(tweetData.created_at)}</p>
                   <div class="tweet-icons">
                       <i class="fa-solid fa-retweet"></i>
                       <i class="fa-solid fa-heart"></i>
@@ -64,39 +65,14 @@ $(document).ready(function() {
 
 
   function renderTweets(tweets) {
+    $('.tweets-container').empty();
     tweets.forEach(tweet => {
         const $tweet = createTweetElement(tweet);
         $('.tweets-container').prepend($tweet);
     });
     timeago().render(document.querySelectorAll('.tweet-time'));
 }
-
-
-    const data = [
-        {
-            "user": {
-                "name": "Newton",
-                "avatars": "https://i.imgur.com/73hZDYK.png",
-                "handle": "@SirIsaac"
-            },
-            "content": {
-                "text": "If I have seen further it is by standing on the shoulders of giants"
-            },
-            "created_at": 1461116232227
-        },
-        {
-            "user": {
-                "name": "Descartes",
-                "avatars": "https://i.imgur.com/nlhLi3I.png",
-                "handle": "@rd"
-            },
-            "content": {
-                "text": "Je pense , donc je suis"
-            },
-            "created_at": 1461113959088
-        }
-    ];
     
-    renderTweets(data);
+    loadTweets();
 });
 
