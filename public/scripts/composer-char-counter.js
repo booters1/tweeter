5adf85bcf0932f98f8479f4934bd18b4cdde878c
Ryan Huynh <rhuynh7@hotmail.com>
2024-04-09 11:56:01 -0600

    fixed counter to dynamically update with input not key up

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,8 @@
 //reference: https://stackoverflow.com/a/7934560
 $(document).ready(function() {
-  $(".new-tweet textarea").on("keyup", function() {
+  $(".new-tweet textarea").on("input", function() {
     //retrieve length of ea line
+
     var inputLength = $(this).val().length;
     var remainingChars = 140 - inputLength;
     var counter = $(this).closest('form').find('.counter');
